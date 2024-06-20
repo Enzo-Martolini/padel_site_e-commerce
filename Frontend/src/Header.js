ab577f60b13d90bcd8bc8b497629b0f4eabc672c
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-06-20 15:23:35 +0200

    push file txt

---
+++ Frontend/src/Header.js
@@ -0,0 +1,10 @@
+function AfficherHeader(){
+
+const header= document.createElement('header');
+var linkHeader= document.createElement('a')
+linkHeader.href="https://index.html"
+linkHeader.textContent="Raquettes"
+document.getElementsByName('header').appendChild(linkHeader);
+
+
+}
