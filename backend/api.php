129d0716ecc2ce6d855dbe6e1e10f2fb00a152d9
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-06-26 11:56:20 +0200

    update function connexion

--- backend/api.php
+++ backend/api.php
@@ -13,6 +13,7 @@ function addUser ($name, $surname, $mail, $password) {
 
     //Execution de la requête
     if ($stmt->execute()){
+        connexion($mail, $password);
         echo '<script>console.log("enregistrement réussi")</script>';
     } else {
         echo '<script>console.log("Erreur lors de l\'enregistrement : ' . $mysqli->error . '</script>';
@@ -42,31 +43,39 @@ function getUserById ($id){
 
 function connexion ($mail, $password){
 
+    $passwordError = false;
+    $mailError = false;
+
     global $mysqli;
 
     $sql = "SELECT * FROM USER WHERE mail=?";
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param('s', $mail);
-    $result = $stmt->execute();
+    $stmt->execute();
 
-    if ($result)
+    $result = $stmt->get_result();  
+    $user = $result->fetch_assoc();
+
+    if ($user!== null)
     {
         echo '<script>console.log("Utilisateur trouvé")</script>';
-        $result = $stmt->get_result();  
-        $result = $result->fetch_assoc();
-        if ($result['password']===$password){
+        if ($user['password']===$password){
             session_start();
-            $_SESSION['id'] = $result['id'];
-            $_SESSION['name'] = $result['name'];
-            $_SESSION['surname'] = $result['surname'];
-            $_SESSION['mail'] = $result['mail'];
-            header("Location: ../index.php");
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['name'] = $user['name'];
+            $_SESSION['surname'] = $user['surname'];
+            $_SESSION['mail'] = $user['mail'];
+            header("Location: profil.php");
+        } else {
+            echo '<script>console.log("mot de passe incorect"</script>';
+            return true;
         }
     } else {
-        echo '<script>console.log("aucun utilisateur trouvé")</script>';
+        echo '<script>console.log("mail incorrect"</script>';
+        return true;
     }
-
 }
+
 
 function getProduct (){
 
@@ -88,4 +97,95 @@ function getProduct (){
     $mysqli->close();
 
     return $data;
+}
+
+function getCommand($id) {
+
+    global $mysqli;
+
+    $sql = "SELECT * FROM command WHERE id_user = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('s', $id);
+    $stmt->execute();
+
+    $result = $stmt->get_result();  
+
+    if ($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            $data[]=$row;
+        }
+    } else {
+        echo "Pas de resultats";
+    }
+
+    if (isset($data)) { 
+        return $data;
+    }
+}
+
+function getHistoric ($id_command){
+
+    global $mysqli;
+
+    $sql = "SELECT * FROM historical WHERE id_command = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('s', $id_command);
+    $stmt->execute();
+
+    $result = $stmt->get_result();  
+
+    if ($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            $data[]=$row;
+        }
+    } else {
+        echo "Pas de resultats";
+    }
+
+    if (isset($data)) { 
+        return $data;
+    }
+
+}
+
+function changePassword($id_user, $password, $newPassword, $verifyPassword){
+
+    global $mysqli;
+    $data = [];
+
+    $sql = "SELECT password FROM user WHERE id = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('s', $id_user);
+    $stmt->execute();
+
+    $result = $stmt->get_result();  
+
+    if ($result->num_rows>0){
+        while($row = $result->fetch_assoc()){
+            $data[]=$row;
+
+            if ($data[0]['password']===$password){
+                if ($newPassword===$verifyPassword){
+                    $sql = "UPDATE user SET password = ? WHERE id = ?";
+                    $stmt = $mysqli->prepare($sql);
+                    $stmt->bind_param('ss', $newPassword, $id_user);
+
+                    if ($stmt->execute()){
+                        echo '<p style="color:green; margin-left:0%">Le mot de passe a été mis à jour</p>';
+                    };
+                } else {
+                    echo '<p style="color:red; margin-left:0%">Les mot de passe entrés sont différents</p>';
+                }
+            } else {
+                echo '<p style="color:red; margin-left:0%">Mot de passe invalide</p>';
+            }
+        }
+    } else {
+        echo "Pas de resultats";
+    }
+
+
+
+
+
 }
